bf21300d56c8e5a1762e80b13f643da2613f9e49
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-07-24 16:09:59 +0530

    Add files via upload

--- 7.Dashboard_meeting_magement/script.js
+++ 7.Dashboard_meeting_magement/script.js
@@ -1,3 +1,16 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('sidebar').classList.toggle('closed');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const statusElement = document.getElementById("status");
+    const submitBtn = document.getElementById("submitBtn");
+
+    const statuses = ["Upcoming", "Published", "In Progress", "Cancelled"];
+    let currentIndex = 0;
+
+    submitBtn.addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % statuses.length;
+        statusElement.textContent = statuses[currentIndex];
+    });
+});
