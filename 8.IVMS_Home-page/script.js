41ed968ccb935f9e963926189e3047c1803f0c48
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-07-25 17:06:43 +0530

    Add files via upload

--- 8.IVMS_Home-page/script.js
+++ 8.IVMS_Home-page/script.js
@@ -1,8 +1,36 @@
 // script.js
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
+});
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > window.innerHeight) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    function animateCounter(element, start, end, duration) {
+        let startTimestamp = null;
+        const step = timestamp => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            element.innerText = Math.floor(progress * (end - start) + start);
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    }
+
+    document.querySelectorAll('.counter-number').forEach(counter => {
+        animateCounter(counter, 0, parseInt(counter.getAttribute('data-count')), 2000);
+    });
 });
